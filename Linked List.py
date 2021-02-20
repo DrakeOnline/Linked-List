6e10d6e07766df95e3516bef8bf6fa2ee483f0af
Drake <drakec1997@gmail.com>
2021-02-19 19:40:57 -0800

    Initial commit

---
+++ Linked List.py
@@ -0,0 +1,6 @@
+# |========================================================|
+# |    Title:      Linked List.py                          |
+# |    Author:     Drake G. Cummings                       |
+# |    Purpose:    Creating a Linked List                  |
+# |    Date:       February 19th, 2021                     |
+# |========================================================|
---
+++ Main.py
@@ -0,0 +1,6 @@
+# |========================================================|
+# |    Title:      main.py                                 |
+# |    Author:     Drake G. Cummings                       |
+# |    Purpose:    Driver file for Linked List             |
+# |    Date:       February 19th, 2021                     |
+# |========================================================|
