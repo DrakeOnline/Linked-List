c910b5b4b078f6b7b731a68d3da8a0f44ebb4d77
Drake <drakec1997@gmail.com>
2021-04-19 13:21:17 -0700

    Methods for the linked list to see if there are cycles

--- Main.py
+++ Main.py
@@ -32,7 +32,6 @@ print()
 
 # append_right test
 print("append_right test:")
-test.append_right(8)
 test.append_right(24)
 test.append_right(19)
 test.append_right(20)
@@ -72,6 +71,71 @@ print(test)
 print("After reverse")
 test.reverse()
 print(test)
+
+print(divider)
+print()
+
+
+# Tortiose and Hare test
+print("Tortiose and Hare")
+print("Test without pattern")
+print(test)
+test.tortoise_and_hare()
+print()
+print("Test with pattern")
+test.append_right(8)
+test.append_right(1)
+test.append_right(6)
+test.append_right(8)
+test.append_right(1)
+test.append_right(6)
+test.append_right(8)
+test.append_right(1)
+test.append_right(6)
+test.append_right(8)
+test.append_right(1)
+test.append_right(6)
+print(test)
+test.tortoise_and_hare()
+
+print(divider)
+print()
+
+
+# Circular test
+print("Test if circular")
+print("Test on linear")
+test.is_circular()
+print()
+print("Test on circle")
+test.make_circular()
+test.is_circular()
+
+print(divider)
+print()
+
+
+# In order to make linear again
+test = LinkedList(7)
+test.append_right(8)
+test.append_right(1)
+test.append_right(6)
+test.append_right(8)
+test.append_right(1)
+test.append_right(6)
+test.append_right(8)
+test.append_right(1)
+print()
+
+# Lollipop test
+print("Test if Lollipop")
+print("Test on linear")
+test.is_lollipop()
+print()
+print("Test on lollipop")
+# Make lollipop
+test.get_tail().next = test.head.next.next.next
+test.is_lollipop()
 
 print(divider)
 print()
